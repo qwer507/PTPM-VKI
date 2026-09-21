fd82d0283ca40799f4b818b621f98a561b3748d2
bykovam <bykovam@mer.ci.nsu.ru>
2026-09-21 12:16:37 +0700

    in work

--- GetPointsTriangle.py
+++ GetPointsTriangle.py
@@ -1,16 +1,20 @@
+import math
+
+
 class CalcTriangle:
     def GetPointsAndType(self):
-        FirstSide = float(input("ВВедите размер первой стороны: "))
-        SecondSide = float(input("ВВедите размер второй стороны: "))
-        ThirdSide = float(input("ВВедите размер третье стороны: "))
-        return
+        try:
+            firstSide = float(input("ВВедите размер первой стороны: "))
+            secondSide = float(input("ВВедите размер второй стороны: "))
+            thirdSide = float(input("ВВедите размер третье стороны: "))
+            return
+        except ValueError:
+            return ("",[(-2,-2),(-2,-2),(-2,-2)])
 
     def ItExist(self,s1,s2,s3):
-        if(s1 + s2 < s3):
+        if(s1 + s2 <= s3 or s1 + s3 <= s2 or s2 + s3 <= s1):
             return False
-        elif(s1 + s3 < s2):
-            return False
-        elif(s2 + s3 < s1):
+        elif(s1 <= 0 or s2 <= 0 or s3 <= 0):
             return False
         return True
 
@@ -19,11 +23,21 @@ class CalcTriangle:
             return "Не треугольник"
         elif(s1 == s2 == s3):
             return "Равносторонний"
-        elif(s1 != s2 & s2 != s3 & s1 != s3): # Нужно попробывать (s1 != s2 != s3)
+        elif(s1 != s2 and s2 != s3 and s1 != s3):
             return "Разносторонний"
         return "Равнобедренный"
 
     def CalcPoints(self, s1, s2, s3):
         if (not self.ItExist(s1, s2, s3)):
             return [(-1,-1),(-1,-1),(-1,-1)]
+
+        a = (0,0)
+        b = (s3,0)
+        cos_a = (s2**2 + s3**2 - s1**2) / (2 * s2 * s3)
+        cos_a = max(-1.0, min(1.0, cos_a))
+        c = (s2 * cos_a, s2 * math.sqrt(1 - cos_a**2))
+        return [a,b,c]
+
+    def CanPutInSquare(self,a,b,c,size=100):
+
 
